20c72a4b46297a12051626f6ec89b670684feac6
David Crenin <david.crenin@gmail.com>
2019-10-14 11:46:54 +0200

    test

--- vitrine/urls.py
+++ vitrine/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from .views import HomePageView
+from .views import emailView, successView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('email/', emailView, name='email'),
+    path('success/', successView, name='success'),
 ]
