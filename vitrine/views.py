aa7f83805428c301da7d8106440cd1b2fd3796cc
David Crenin <david.crenin@gmail.com>
2019-11-07 14:23:39 +0100

    test

--- vitrine/views.py
+++ vitrine/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
 from .models import Actualite, Equipe
 from datetime import datetime
 
@@ -55,10 +55,11 @@ def emailView(request):
             
             try:
                 send_mail(subject, message, from_email, ['david.crenin@gmail.com'])
+                messages.success(request, 'Your password was updated successfully!')
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('home')
-    return render(request, "contact.html", {'form': form})
+    return render(request, "contact.html", {'form': form},)
 
 def successView(request):
     return HttpResponse('Success! Thank you for your message.')
